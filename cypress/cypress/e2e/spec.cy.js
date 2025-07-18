c351893b7ff163b92347022b53f37f2a2ff32030
Anna Ristau <annaristau@MacBookPro.attlocal.net>
2025-07-18 11:11:46 -0400

    Refresh on fixture files and custom commands

--- cypress/cypress/e2e/spec.cy.js
+++ cypress/cypress/e2e/spec.cy.js
@@ -2,11 +2,29 @@ import HomePage from "../support/pageObjects/HomePage";
 
 const homePage = new HomePage();
 
-describe('smoke test', () => {
+//Load Fixture File
+before(function () {
+  cy.fixture('example.json').then(function (data) {
+    this.example = data;
+  });
+});
 
-  it('Home page shows correct title', () => {
+describe('smoke test', function() {
+
+  it('Home page shows correct title', function() {
     homePage.navigate();
     homePage.elements.pageTitle().should("include", "Kitchen Sink");
+  });
+
+  it('Fills out email field with fixture file', function() {
+    cy.visit('https://example.cypress.io/commands/actions');
+    cy.get("#email1").type(this.example.email);
+
+  });
+
+   it('Clicks on a link with a command', function() {
+    homePage.navigate();
+    cy.clickLink("type");
   });
 
 
